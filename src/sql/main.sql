aa84dc0959d8b472e3d929fb6e11eaca8f5a1bea
Sailesh Dahal <saileshbro@gmail.com>
2019-07-10 22:02:40 +0545

    change datatypes

--- src/sql/main.sql
+++ src/sql/main.sql
@@ -1,23 +1,23 @@
 CREATE TABLE food_category
 (
     food_category_id int(12) not null,
-    food_category_name char(50),
+    food_category_name varchar(100),
     primary key(food_category_id)
 );
 
 CREATE TABLE food_item
 (
-    food_item_id varchar(10) not null,
-    name char(18),
-    price int(7),
-    food_category_id int(12),
+    food_item_id int(10) not null,
+    name varchar(100),
+    price int(10),
+    food_category_id int(10),
     primary key(food_item_id),
     FOREIGN KEY (food_category_id) REFERENCES food_category(food_category_id) ON DELETE CASCADE
 );
 
 CREATE TABLE menu
 (
-    menu_id varchar(12) not null,
+    menu_id int(11) not null,
     start_date date,
     end_date date,
     is_active int(1),
@@ -25,9 +25,9 @@ CREATE TABLE menu
 );
 CREATE TABLE menu_content
 (
-    menu_id varchar(12) not null,
-    food_item_id char(20),
-    is_available char(5),
+    menu_id int(11) not null,
+    food_item_id int(10),
+    is_available int(1),
     FOREIGN KEY (food_item_id) REFERENCES food_item(food_item_id) ON DELETE CASCADE,
     FOREIGN KEY(menu_id) REFERENCES menu(menu_id)
 );

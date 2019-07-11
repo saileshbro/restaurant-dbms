e0b4fb3cb6733f04d122df69e5984943afd2f018
Sarayu <sarayugautam1@gmail.com>
2019-07-11 09:08:53 +0545

    added tables

--- src/sql/main.sql
+++ src/sql/main.sql
@@ -28,3 +28,64 @@ CREATE TABLE menu_content(
     FOREIGN KEY(menu_id) REFERENCES menu(menu_id)
 );
 
+create table if not exists manager(
+    username varchar(50) primary key,
+    password varchar(255),
+    name varchar(100));
+
+create table if not exists staff_cat(
+	staff_cat_id int(11) PRIMARY key AUTO_INCREMENT,
+    category varchar(100) UNIQUE,
+    salary double(10,2)  
+);
+
+CREATE table if not EXISTS import_company(
+	import_comp_id int(11) PRIMARY key AUTO_INCREMENT,
+    name varchar(128),
+    address varchar(255),
+    total_trans double(10,2),
+    remain_trans double(10,2),
+    purchase_type varchar(128)
+);
+Create table if not exists restaurant(
+    rest_id int(11) PRIMARY key AUTO_INCREMENT,
+    name varchar(255),
+    address varchar(255),
+    total_staff int(11),
+    capacity int(11),
+    total_tables int(11)   
+);
+create table if not EXISTS staff(
+   staff_id int(11) PRIMARY key AUTO_INCREMENT,
+    name varchar(150),
+   address varchar(255),
+    staff_cat_id int(11),
+    last_paid_date date,
+    joined_date date,
+    FOREIGN key (staff_cat_id) references staff_cat(staff_cat_id) on DELETE CASCADE);
+
+create table if not EXISTS rest_table(
+    table_id int(11) PRIMARY key AUTO_INCREMENT,
+    is_empty bit,
+    date_time Date,
+    table_no int(11));
+
+create table if not exists import(
+    import_id int(11) primary key AUTO_INCREMENT,
+    import_comp_id int(11),
+    good varchar(255),
+    bill_no int(11),
+    quantiy varchar(80),
+    total_price_rs double(10,2),
+    import_date date,
+    FOREIGN key (import_comp_id) references import_company(import_comp_id)
+);
+
+create table if not exists stock(
+	stock_id int(11) PRIMARY key AUTO_INCREMENT,
+	type_of_stock varchar(80),
+    name varchar(120),
+    import_id int(11),
+    quantity varchar(80),
+    FOREIGN key (import_id) REFERENCES import(import_id)
+);

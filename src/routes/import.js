00e41e0767402d4bfaa66c22db9b82d95b91f6a9
Sarayu <sarayugautam1@gmail.com>
2019-07-13 09:43:18 +0545

    middleware added

--- src/routes/import.js
+++ src/routes/import.js
@@ -1,3 +1,9 @@
 const router = require('express').Router;
-
+const { contactValidator } = require("../middlewares/contactvalidator");
+const { credentialsValidator } = require("../middlewares/credentialvalidator");
+const importController = require('../controllers/importController');
+router.post('/import/addcompany', contactValidator, importController.addImportCompany);
+router.get('/import/getcompanies', importController.getImportCompanies);
+router.patch('/import/updatecompany/:import_company_id', contactValidator, importController.updateImportCompany);
+router.delete('/import/deletecompany/:import_company_id', importController.deleteImportCompany);
 module.exports = router;

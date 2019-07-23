12edb7a15c836b857fff7f6e2d6887d049ef35a0
Sailesh Dahal <saileshbro@gmail.com>
2019-07-23 23:47:09 +0545

    .

--- src/controllers/billController.js
+++ src/controllers/billController.js
@@ -1,85 +1,94 @@
-const pool = require('../database/database');
+const pool = require("../database/database");
 const { generateId } = require("../functions/id");
 module.exports.createBill = async (req, res) => {
-    const {
-        issue_date
-    } = req.body;
-    const bill_no = generateId("BILL");
-    const { order_id } = req.params;
-    if (bill_no == 0 || !bill_no || order_id.length == 0 || !order_id) {
-        return res.send({ error: "Bill number and order id not provided" });
+  const { issue_date } = req.body;
+  const bill_no = generateId("BILL");
+  const { order_id } = req.params;
+  if (bill_no == 0 || !bill_no || order_id.length == 0 || !order_id) {
+    return res.send({ error: "Bill number and order id not provided" });
+  }
+  try {
+    const total_price = await pool.query(
+      "SELECT SUM(food_item.food_item_price) as total_price from food_item INNER JOIN order_item ON food_item.food_item_name=order_item.food_item_name where order_item.order_id=?",
+      [order_id]
+    );
+    const insertbill = await pool.query(
+      `INSERT INTO bill SET bill_no=?,issue_date=?,order_id=?,total_price=?`,
+      [bill_no, issue_date, order_id, total_price]
+    );
+    console.log(insertbill);
+    if (insertbill.affectedRows == 1) {
+      return res.send({ message: "Bill successfully created." });
+    } else {
+      return res.status(400).send({ error: "Unable to create bill." });
     }
-    try {
-        const total_price = await pool.query("SELECT SUM(food_item.food_item_price) as total_price from food_item INNER JOIN order_item ON food_item.food_item_name=order_item.food_item_name where order_item.order_id=?", [order_id]);
-        const insertbill = await pool.query(
-            `INSERT INTO bill SET bill_no=?,issue_date=?,order_id=?,total_price=?`,
-            [bill_no, issue_date, order_id, total_price]
-        );
-        console.log(insertbill);
-        if (insertbill.affectedRows == 1) {
-            return res.send({ message: "Bill successfully created." });
-        } else {
-            return res.status(400).send({ error: "Unable to create bill." });
-        }
-
-    } catch (error) {
-        if (error.code === "ER_DUP_ENTRY") {
-            return res.status(500).send({
-                error: "Bill with given credentials already exists."
-            });
-        } else {
-            return res.status(500).send({ error });
-        }
+  } catch (error) {
+    if (error.code === "ER_DUP_ENTRY") {
+      return res.status(500).send({
+        error: "Bill with given credentials already exists."
+      });
+    } else {
+      return res.status(500).send({ error });
     }
-}
+  }
+};
 
 module.exports.getBill = async (req, res) => {
-    const { bill_no } = req.params;
-    if (!bill_no || bill_no.length == 0) {
-        return res.send({ message: "Bill number must be specified" });
+  const { bill_no } = req.params;
+  if (!bill_no || bill_no.length == 0) {
+    return res.send({ message: "Bill number must be specified" });
+  }
+  try {
+    const getBill = await pool.query(
+      "SELECT bill_no,issue_date,order_id,total_price FROM bill WHERE bill_no=? ",
+      [bill_no]
+    );
+    if (getBill.affectedRows != 0) {
+      return res.send({ bill: getBill });
+    } else {
+      return res.status(400).send({ error: "Bill couldn't be obtained" });
     }
-    try {
-        const getBill = await pool.query("SELECT bill_no,issue_date,order_id,total_price FROM bill WHERE bill_no=? ", [bill_no]);
-        if (getBill.affectedRows != 0) {
-            return res.send({ bill: getBill });
-        } else {
-            return res.status(400).send({ error: "Bill couldn't be obtained" });
-        }
-    } catch (error) {
-        return res.status(500).send({ error: "Internal server error." });
-    }
-}
+  } catch (error) {
+    return res.status(500).send({ error: "Internal server error." });
+  }
+};
 
 module.exports.getBillByOrder = async (req, res) => {
-    const { order_id } = req.params;
-    if (!order_id || order_id.length == 0) {
-        return res.send({ message: "Order id must be specified" });
+  const { order_id } = req.params;
+  if (!order_id || order_id.length == 0) {
+    return res.send({ message: "Order id must be specified" });
+  }
+  try {
+    const getBill = await pool.query(
+      "SELECT bill_no,issue_date,order_id,total_price FROM bill WHERE order_id=? ",
+      [order_id]
+    );
+    if (getBill.affectedRows != 0) {
+      return res.send({ bill: getBill });
+    } else {
+      return res.status(400).send({ error: "Bill couldn't be obtained" });
     }
-    try {
-        const getBill = await pool.query("SELECT bill_no,issue_date,order_id,total_price FROM bill WHERE order_id=? ", [order_id]);
-        if (getBill.affectedRows != 0) {
-            return res.send({ bill: getBill });
-        } else {
-            return res.status(400).send({ error: "Bill couldn't be obtained" });
-        }
-    } catch (error) {
-        return res.status(500).send({ error: "Internal server error." });
-    }
-}
+  } catch (error) {
+    return res.status(500).send({ error: "Internal server error." });
+  }
+};
 
 module.exports.getBillByDate = async (req, res) => {
-    const { issue_date } = req.params;
-    if (!issue_date || issue_date.length == 0) {
-        return res.send({ message: "Issued date of bill must be specified" });
+  const { issue_date } = req.params;
+  if (!issue_date || issue_date.length == 0) {
+    return res.send({ message: "Issued date of bill must be specified" });
+  }
+  try {
+    const getBill = await pool.query(
+      "SELECT bill_no,issue_date,order_id,total_price FROM bill WHERE bill_no=? ",
+      [issue_date]
+    );
+    if (getBill.affectedRows != 0) {
+      return res.send({ bill: getBill });
+    } else {
+      return res.status(400).send({ error: "Bill couldn't be obtained" });
     }
-    try {
-        const getBill = await pool.query("SELECT bill_no,issue_date,order_id,total_price FROM bill WHERE bill_no=? ", [issue_date]);
-        if (getBill.affectedRows != 0) {
-            return res.send({ bill: getBill });
-        } else {
-            return res.status(400).send({ error: "Bill couldn't be obtained" });
-        }
-    } catch (error) {
-        return res.status(500).send({ error: "Internal server error." });
-    }
-}
+  } catch (error) {
+    return res.status(500).send({ error: "Internal server error." });
+  }
+};

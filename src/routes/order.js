0568177e4308455fe56d43a9fef172590a5c8862
Sailesh Dahal <saileshbro@gmail.com>
2019-07-23 22:00:17 +0545

    orders

--- src/routes/order.js
+++ src/routes/order.js
@@ -5,8 +5,11 @@ router.post(
   "/orders/create/:staff_id/:table_no",
   orderController.createTableOrder
 );
+router.patch("/order/:order_id", orderController.addIntoOrder);
 router.post("/orders/makecomplete/:order_id", orderController.completeAOrder);
 router.get("/orders", orderController.showOrders);
 router.get("/orders/table/:table_no", orderController.getTableOrders);
+router.get("/orders/staff/:staff_id", orderController.getOrdersByStaff);
+router.get("/orders/customer/:customer_id", orderController.getCustomerOrders);
 router.delete("/order/:order_id", orderController.deleteOrder);
 module.exports = router;
